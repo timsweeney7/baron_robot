afb45f6f30a74f72fae7aeef9c8d2905d216168b
Tim Sweeney <timsweeney97@gmail.com>
2025-04-19 18:01:35 -0400

    Finished hw9 - collection.  Collection is done by continuously moving closer to a block in increments. No distance calculations are done.

--- HW9/distance_calibration.py
+++ HW9/distance_calibration.py
@@ -2,9 +2,11 @@
 Used for calibrating the distance of a block from the robot
 """
 
-from utilities.vision import camera
+from utilities.vision import Camera
+import cv2 as cv
+import matplotlib.pyplot as plt
 
-cam = camera
+cam = Camera()
 areas = []
 distances = []
 
@@ -14,11 +16,24 @@ while True:
     if key == 'q':
         break
     rgb_image = cam.capture_image()
+    cv.imwrite("rgb_image.jpg", cv.cvtColor(rgb_image, cv.COLOR_RGB2BGR))
     block_image, blocks = cam.find_blocks(rgb_image)
-    area = blocks[0].bounding_area
-    distance = float(input("Measured distance of block: "))
-    
-    areas.append()
+    cv.imwrite("boxedImageEnd.jpg", cv.cvtColor(block_image, cv.COLOR_RGB2BGR))
+    if blocks != None:
+        area = blocks[0].bounding_area
+        distance = float(input("Measured distance of block (inches): "))
+        if distance == '':
+            continue
+        distance = distance/39.37
+        areas.append(area)
+        distances.append(distance)
+    print()
     
 # start plottin
-    
+print(f"Areas length: {len(areas)}")
+print(f"Distances length: {len(distances)}")
+
+plt.scatter(distances, areas)
+plt.title("Distances vs Bounding Box Area")
+plt.xlabel("Distance (meters)")
+plt.savefig("distance_area_graph.jpg")
